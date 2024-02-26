55d7aceb8a786fa9695750160b44daa1f9ee8814
chrisrathana <160774578+chrisrathana@users.noreply.github.com>
2024-02-27 00:06:11 +0700

    google-search.js
    Signed-off-by: chrisrathana <160774578+chrisrathana@users.noreply.github.com>

--- Website718001/index.js
+++
@@ -1,8 +0,0 @@
- function showreview(auto){
-       XMLHttpRequest()
-          var gaJsHost = (("https:" == document.location.protocol) ? "https://ssl." : "https://www.");
-        console.log("hello would")
-    DataView()
- }
-
-    
